8e5b5752e34e026a45b7983dc9f5726072a133dd
Adrian Las <alasadam1b@gmail.com>
2023-03-08 14:10:58 +0100

    Change colors

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData lightTheme() {
     ColorScheme scheme =
-        ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 145, 0));
+        ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 196, 0));
 
     return ThemeData(colorScheme: scheme, useMaterial3: true);
   }
 
   static ThemeData darkTheme() {
     ColorScheme scheme = ColorScheme.fromSeed(
-        seedColor: Color.fromARGB(255, 0, 174, 255),
+        seedColor: const Color.fromARGB(255, 17, 0, 255),
         brightness: Brightness.dark);
 
     return ThemeData(
